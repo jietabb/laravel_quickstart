259a28d2bb1dd7aeb7e5c3f8a736ae98b43c99e0
jietabb <wanghuajie@x-service.co.jp>
2022-11-25 00:04:45 +0900

    update styles

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -37,48 +37,35 @@
             <div class="panel-heading">
                 Current Tasks
             </div>
-
             <div class="panel-body">
                 <table class="table table-striped task-table">
-
                     <!-- Table Headings -->
                     <thead>
                         <th>Task</th>
                         <th>&nbsp;</th>
                     </thead>
-
                     <!-- Table Body -->
                     <tbody>
                         @foreach ($tasks as $task)
-                            <tr>
-                                <!-- Task Name -->
-                                <td class="table-text">
-                                    <div>{{ $task->name }}</div>
-                                </td>
+                        <tr>
+                            <!-- Task Name -->
+                            <td class="table-text">
+                                <div>{{ $task->name }}</div>
+                            </td>
 
-                                <td>
-                                    <!-- TODO: Delete Button -->
-                                </td>
-                            </tr>
+                            <!-- Delete Button -->
+                            <td>
+                                <form action="/task/{{ $task->id }}" method="POST">
+                                    {{ csrf_field() }}
+                                    {{ method_field('DELETE') }}
+                                    <button>Delete Task</button>
+                                </form>
+                            </td>
+                        </tr>
                         @endforeach
                     </tbody>
                 </table>
             </div>
         </div>
     @endif
-    <tr>
-        <!-- Task Name -->
-        <td class="table-text">
-            <div>{{ $task->name }}</div>
-        </td>
-
-        <!-- Delete Button -->
-        <td>
-            <form action="/task/{{ $task->id }}" method="POST">
-                {{ csrf_field() }}
-                {{ method_field('DELETE') }}
-                <button>Delete Task</button>
-            </form>
-        </td>
-    </tr>
 @endsection
